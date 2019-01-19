6b8a9fb6c5ac1e0f3b2d99ddbc0dccbb815168b0
Ivan Pankov <ivan.pankov.365@yandex.ru>
2019-01-20 01:01:35 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+});
+
+$(document).ready(function() {
+    $('input#input_text, textarea#textarea2').characterCounter();
+});
+
+$(document).ready(function(){
+    $('.modal').modal();
+});
